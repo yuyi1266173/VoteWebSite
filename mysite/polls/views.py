2df83b4c5a55421c55238fc32e6fbafe3535dd70
wuyu <wuyu@xinktech.com>
2018-12-26 21:20:55 +0800

    issue #2 use template in polls detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Question
 
@@ -19,7 +20,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
